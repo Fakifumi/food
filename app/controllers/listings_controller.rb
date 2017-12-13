5bc2d64016219e1be7128413a9d0599ae345ee39
fujishiro akifumi <YOUREMAIL@gmail.com>
2017-12-13 20:26:11 +0900

    change live key

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,6 +2,7 @@ class ListingsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_listing, only: [:show, :update, :basics, :description, :address, :price, :photos, :calendar, :bankaccount, :publish]
   before_action :access_deny, only: [:basics, :description, :address, :price, :photos, :calendar, :bankaccount, :publish]
+    
   def index
     @listings = current_user.listings
   end
